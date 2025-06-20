ab3d5373da44a899d7e1e86ecd88d9d2715c3c71
Mbg1686 <chadautohouserental@gmail.com>
2025-06-20 18:42:00 +0200

    envoie de modification Images

--- chadauto_project/settings.py
+++ chadauto_project/settings.py
@@ -31,6 +31,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     
     # Applications personnalisées
     'core',
